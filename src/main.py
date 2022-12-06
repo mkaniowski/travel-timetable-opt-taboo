02ad6ebc4b6bbfc00bb892b3cab96ae89a2b4724
mkaniowski <michal.kaniowski2001@gmail.com>
2022-12-06 19:57:20 +0100

    fitness and ex sol

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import geopy.distance
 
 from city import City
+from vehicle import Vehicle
 from randTimetable import randTimetable
 
 
@@ -12,11 +13,15 @@ class Optimize:
         self.cities = []
         self.names = []
         self.dists = None
+        self.vehicles = []
 
     # add city object to the list of cities
     def addCity(self, city):
         self.cities.append(City(city['city'], city['lat'], city['lon']))
         self.names.append(city['city'])
+
+    def addVehicle(self, vehicle):
+        self.vehicles.append(vehicle)
 
     # calcualte distance bewteen 2 cities and put it in matrix
     def calculateDist(self):
@@ -46,11 +51,23 @@ class Optimize:
 
     # calculate fitness of the solution
     def fitness(self, sol):
-        return None
+        # sol -> [(t, type, city, additionalTime), (t, type, city, additionalTime), ...], where t is the time spent for transport (full hours) to city and type (0, 1, 2) is the type of transport. Additional time is time needed to full hour + time spent in city
+        out = 0
+        for i in range(len(sol)-1):
+            if i == 0:
+                continue
+            deltaTime = sol[i-1][0] - sol[i][0]
+            out += deltaTime*self.vehicles[sol[i][1]].cost + sol[i][3]
+        return out
 
 
 def main():
     opt = Optimize()
+    opt.addVehicle(Vehicle('bus', 100, 1))
+    opt.addVehicle(Vehicle('train', 160, 1.2))
+    opt.addVehicle(Vehicle('plane', 800, 2.5))
+
+    # [bus, train, plane] order in timetable
 
     # adding cities to object
     with open(f'{os.getcwd()}/src/data/cities.json', 'r') as f:
@@ -64,6 +81,11 @@ def main():
     randTimetable()
 
     opt.loadTimetable()
+
+    ex = [(0, 0, 'warsaw', 0), (5, 0, 'berlin', 4), (4, 0, 'prague', 5),
+          (2, 2, 'rome', 4), (2, 2, 'paris', 4), (4, 1, 'amsterdam', 3)]
+
+    print(opt.fitness(ex))
 
 
 main()
---
+++ src/vehicle.py
@@ -0,0 +1,5 @@
+class Vehicle:
+    def __init__(self, type, vel, cost):
+        self.type = type
+        self.vel = vel
+        self.cost = cost
