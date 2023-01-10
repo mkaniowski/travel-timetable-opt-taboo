416a99c69b18687b4b8161b3466de8de09ee695b
mkaniowski <michal.kaniowski2001@gmail.com>
2023-01-10 02:46:03 +0100

    gui and fixes

--- src/main.py
+++ src/main.py
@@ -1,242 +1,226 @@
-import math
+import tkinter as tk
 import os
 import json
-import numpy as np
-import geopy.distance
-
-from city import City
-from vehicle import Vehicle
 from randTimetable import randTimetable
+from optimize import Optimize
+from vehicle import Vehicle
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 
-class Optimize:
-    def __init__(self):
-        self.cities = []
-        self.names = []
-        self.dists = None
-        self.vehicles = []
+root = tk.Tk()
+root.title('App')
+root.geometry("1300x300")
 
-    # add city object to the list of cities
-    def addCity(self, city, timeSpent, desire):
-        self.cities.append(
-            City(city['city'], city['lat'], city['lon'], timeSpent, desire))
-        self.names.append(city['city'])
+txt = ""
 
-    def addVehicle(self, vehicle):
-        self.vehicles.append(vehicle)
+startPointVar=tk.StringVar(value="0")
+endPointVar=tk.StringVar()
+startTimeVar=tk.StringVar()
+maxCostVar=tk.StringVar()
+maxTabooLenVar=tk.StringVar()
+aspirationVar=tk.StringVar(value="0")
+nmaxVar=tk.StringVar()
+busVVar=tk.StringVar(value="100")
+busCostVar=tk.StringVar(value="1")
+trainVVar=tk.StringVar(value="160")
+trainCostVar=tk.StringVar(value="1.2")
+planeVVar=tk.StringVar(value="800")
+planeCostVar=tk.StringVar(value="5")
 
-    # calcualte distance bewteen 2 cities and put it in matrix
-    def calculateDist(self):
-        self.dists = np.zeros((len(self.cities), len(self.cities)))
+startPointlabel = tk.Label(root, text = 'Start point', font=('calibre',10, 'bold'))
+startPointEntry = tk.Entry(root, textvariable = startPointVar, font=('calibre',10,'normal'))
 
-        for row in range(len(self.dists)):
-            for col in range(len(self.dists)):
-                if (row == col):
-                    self.dists[row, col] = np.inf
-                    continue
+endPointlabel = tk.Label(root, text = 'End point', font=('calibre',10, 'bold'))
+endPointEntry = tk.Entry(root, textvariable = endPointVar, font=('calibre',10,'normal'))
 
-                lon1 = self.cities[row].lon
-                lon2 = self.cities[col].lon
-                lat1 = self.cities[row].lat
-                lat2 = self.cities[col].lat
+startTimelabel = tk.Label(root, text = 'Start time', font=('calibre',10, 'bold'))
+startTimeEntry = tk.Entry(root, textvariable = startTimeVar, font=('calibre',10,'normal'))
 
-                c1 = (lat1, lon1)
-                c2 = (lat2, lon2)
-                self.dists[row][col] = geopy.distance.geodesic(c1, c2).km
+maxCostlabel = tk.Label(root, text = 'Max cost', font=('calibre',10, 'bold'))
+maxCostEntry = tk.Entry(root, textvariable = maxCostVar, font=('calibre',10,'normal'))
 
-    # load timetables to city objects
-    def loadTimetable(self):
+maxTabooLenlabel = tk.Label(root, text = 'Max taboo len', font=('calibre',10, 'bold'))
+maxTabooLenEntry = tk.Entry(root, textvariable = maxTabooLenVar, font=('calibre',10,'normal'))
+
+aspirationlabel = tk.Label(root, text = 'Aspiration', font=('calibre',10, 'bold'))
+aspirationEntry = tk.Entry(root, textvariable = aspirationVar, font=('calibre',10,'normal'))
+
+nmaxlabel = tk.Label(root, text = 'Max iter', font=('calibre',10, 'bold'))
+nmaxEntry = tk.Entry(root, textvariable = nmaxVar, font=('calibre',10,'normal'))
+
+busVlabel = tk.Label(root, text = 'Bus vel', font=('calibre',10, 'bold'))
+busVEntry = tk.Entry(root, textvariable = busVVar, font=('calibre',10,'normal'))
+
+busCostlabel = tk.Label(root, text = 'Bus cost', font=('calibre',10, 'bold'))
+busCostEntry = tk.Entry(root, textvariable = busCostVar, font=('calibre',10,'normal'))
+
+trainVlabel = tk.Label(root, text = 'Train vel', font=('calibre',10, 'bold'))
+trainVEntry = tk.Entry(root, textvariable = trainVVar, font=('calibre',10,'normal'))
+
+trainCostlabel = tk.Label(root, text = 'Train cost', font=('calibre',10, 'bold'))
+trainCostEntry = tk.Entry(root, textvariable = trainCostVar, font=('calibre',10,'normal'))
+
+planeVlabel = tk.Label(root, text = 'Plane vel', font=('calibre',10, 'bold'))
+planeVEntry = tk.Entry(root, textvariable = planeVVar, font=('calibre',10,'normal'))
+
+planeCostlabel = tk.Label(root, text = 'Plane cost', font=('calibre',10, 'bold'))
+planeCostEntry = tk.Entry(root, textvariable = planeCostVar, font=('calibre',10,'normal'))
+
+
+def editSave():
+    out = {
+        "start": startPointEntry.get(),
+        "end": endPointEntry.get(),
+        "time": startTimeEntry.get(),
+        "maxCost": maxCostEntry.get(),
+        "maxTabooLen": maxTabooLenEntry.get(),
+        "aspiration": aspirationEntry.get(),
+        "nmax": nmaxEntry.get()
+    }
+    print(out)
+    outJSON = json.dumps(out, sort_keys=True, indent=4)
+    with open(f'{os.getcwd()}/src/data/save.json', 'w') as f:
+        f.write(outJSON)
+
+
+def useSave():
+    with open(f'{os.getcwd()}/src/data/save.json', 'r') as f:
+        data = json.load(f)
+    print(data)
+
+    startPointEntry.delete(0, 'end')
+    startPointEntry.insert(0, data['start'])
+
+    endPointEntry.delete(0, 'end')
+    endPointEntry.insert(0, data['end'])
+
+    startTimeEntry.delete(0, 'end')
+    startTimeEntry.insert(0, data['time'])
+
+    maxCostEntry.delete(0, 'end')
+    maxCostEntry.insert(0, data['maxCost'])
+
+    maxTabooLenEntry.delete(0, 'end')
+    maxTabooLenEntry.insert(0, data['maxTabooLen'])
+
+    aspirationEntry.delete(0, 'end')
+    aspirationEntry.insert(0, data['aspiration'])
+
+    nmaxEntry.delete(0, 'end')
+    nmaxEntry.insert(0, data['nmax'])
+
+
+editSaveBtn=tk.Button(root,text = 'Edit saved', command = lambda:editSave())
+useSaveBtn=tk.Button(root,text = 'Use saved', command = lambda:useSave())
+generateBtn=tk.Button(root,text = 'New timetable', command = lambda:randTimetable())
+
+
+def optimize():
+    if isinstance(int(startPointEntry.get()), int) and isinstance(int(endPointEntry.get()), int) and isinstance(int(startTimeEntry.get()), int) and isinstance(float(maxCostEntry.get()), float) and isinstance(int(maxTabooLenEntry.get()), int) and isinstance(int(aspirationEntry.get()), int) and isinstance(int(nmaxEntry.get()), int) and isinstance(float(busVEntry.get()), float) and isinstance(float(busCostEntry.get()), float) and isinstance(float(trainVEntry.get()), float) and isinstance(float(trainCostEntry.get()), float) and isinstance(float(planeVEntry.get()), float) and isinstance(float(planeCostEntry.get()), float):
+
+        opt = Optimize()
+        opt.addVehicle(Vehicle('bus', float(busVEntry.get()), float(busCostEntry.get())))
+        opt.addVehicle(Vehicle('train', float(trainVEntry.get()), float(trainCostEntry.get())))
+        opt.addVehicle(Vehicle('plane', float(planeVEntry.get()), float(planeCostEntry.get())))
+
+        with open(f'{os.getcwd()}/src/data/cities.json', 'r') as f:
+            data = json.load(f)
+
+        for i in data:
+            opt.addCity(i)
+
         with open(f'{os.getcwd()}/src/data/timetable.json', 'r') as f:
             data = json.load(f)
-        for i in range(len(self.cities)):
-            self.cities[i].addTimetable(data[i]['timetable'])
 
-    # calculate cost of the solution
-    def getCost(self, sol):
-        # sol -> [(type, city, t), (type, city, t), ...], where t is additional time spent in city and type (0, 1, 2) is the type of transport
-        out = 0
-        for i in range(len(sol)-1):
-            if i == 0:
-                continue
-            travelTime = self.dists[sol[i-1][1]][sol[i]
-                                                 [1]]/self.vehicles[sol[i][0]].vel
-            res = math.ceil(travelTime) - travelTime
-            out += travelTime*self.vehicles[sol[i][0]].cost*(
-                1/self.cities[sol[i][1]].desire) + self.cities[sol[i][1]].timeSpent + res
-        return out
+        for i in range(len(data)):
+            opt.cities[i].addTimetable(data[i]["timetable"])
 
-    def getInitSol(self, start, end, time):
-        currTime = time
-        visited = [start]
-        curr = start
-        sol = [[0, start, self.cities[start].timeSpent]]
-        addTime = 0
-        while True:
-            for c in range(len(self.cities[curr].timetable[0][currTime])):
-                if (c == curr) or (c in visited) or (c == end):
-                    continue
+        opt.calculateDist()
 
-                while 1 not in self.cities[curr].timetable[0][currTime][c]:
-                    addTime += 1
-                    currTime += 1
+        opt.loadTimetable()
 
-                for t in range(len(self.cities[curr].timetable[0][currTime][c])):
-                    if self.cities[curr].timetable[0][currTime][c][t] == 1:
-                        mov = [t, c, addTime + self.cities[curr].timeSpent]
-                        sol.append(mov)
-                        visited.append(c)
-                        curr = c
-                        addTime = 0
-                        break
-                break
-            if len(sol) > len(self.cities[curr].timetable[0][currTime]) - 2:
-                visited.append(end)
+        o = opt.opt(int(startPointEntry.get()), int(endPointEntry.get()), int(startTimeEntry.get()), float(maxCostEntry.get()), int(maxTabooLenEntry.get()), int(aspirationEntry.get()), int(nmaxEntry.get()))
 
-                while 1 not in self.cities[curr].timetable[0][currTime][end]:
-                    addTime += 1
-                    currTime += 1
+        print(o[:-1])
+        print(o[3:])
 
-                for t in range(len(self.cities[curr].timetable[0][currTime][end])):
-                    if self.cities[curr].timetable[0][currTime][end][t] == 1:
-                        mov = [t, end, addTime + self.cities[curr].timeSpent]
-                        sol.append(mov)
-                        visited.append(end)
-                        curr = c
-                        addTime = 0
-                        break
+        fig = Figure(figsize = (5, 5), dpi = 100)
+        plot1 = fig.add_subplot(111)
+        plot1.grid()
+        plot1.plot(o[3])
 
-                break
-        return sol
+        graph = tk.Tk()
+        graph.title('Graph')
 
-    def getNeighborhood(self, sol):
-        n1 = np.random.randint(low=1, high=len(sol))
-        n2 = np.random.randint(low=1, high=len(sol))
-        while n1 == n2:
-            n2 = np.random.randint(low=1, high=len(sol))
-
-        sol[n1], sol[n2] = sol[n2], sol[n1]
-
-        return sol
-
-    def correctNeighborhood(self, sol, time):
-        currTime = time
-        for i in range(len(sol)):
-            sol[i][2] = self.cities[sol[i][1]].timeSpent
-
-        for i in range(len(sol)-1):
-            if i == 0:
-                continue
-            addTime = 0
-            while 1 not in self.cities[sol[i][1]].timetable[0][currTime][sol[i+1][1]]:
-                addTime += 1
-                currTime += 1
-                if currTime > 23:
-                    currTime = 0
-            sol[i][2] += addTime
-            busCost = np.inf
-            trainCost = np.inf
-            planeCost = np.inf
-            for l in range(3):
-                if self.cities[sol[i][1]].timetable[0][currTime][sol[i+1][1]][l] == 1:
-                    if l == 0:
-                        travelTime = self.dists[sol[i][1]
-                                                ][sol[i+1][1]]/self.vehicles[l].vel
-                        busCost = travelTime * \
-                            self.vehicles[sol[i][0]].cost * \
-                            (1/self.cities[sol[i][1]].desire)
-                    if l == 1:
-                        travelTime = self.dists[sol[i][1]
-                                                ][sol[i+1][1]]/self.vehicles[l].vel
-                        trainCost = travelTime * \
-                            self.vehicles[sol[i][0]].cost * \
-                            (1/self.cities[sol[i][1]].desire)
-                    if l == 2:
-                        travelTime = self.dists[sol[i][1]
-                                                ][sol[i+1][1]]/self.vehicles[l].vel
-                        planeCost = travelTime * \
-                            self.vehicles[sol[i][0]].cost * \
-                            (1/self.cities[sol[i][1]].desire)
-
-            bestVeh = np.argmin([busCost, trainCost, planeCost])
-            sol[i][0] = bestVeh
-
-        return sol
-
-    def opt(self, maxCost: int, maxTabuLen: int, aspiration: int, Nmax: int, time: int):
-        sol = self.getInitSol(2, 6, time)
-        bestSol = sol
-        bestSolCost = self.getCost(bestSol)
-        tabuList = [sol]
-        it = 0
-        noImprovement = 0
-        while ((it <= Nmax) and (bestSolCost <= maxCost)):
-            it += 1
-            newSol = self.correctNeighborhood(self.getNeighborhood(sol), time)
-            if newSol not in tabuList:
-                noImprovement += 1
-                newSolCost = self.getCost(newSol)
-                if newSolCost < bestSolCost:
-                    noImprovement = 0
-                    bestSol = newSol
-                    bestSolCost = newSolCost
-                tabuList.append(newSol)
-                if len(tabuList) >= maxTabuLen:
-                    tabuList.pop(0)
-
-                # aspiration criteria
-                if noImprovement >= aspiration:
-                    aspiredSolCost = self.getCost(tabuList[0])
-                    idx = 0
-                    for i in range(len(tabuList)):
-                        currCost = self.getCost(tabuList[i])
-                        if (currCost < aspiredSolCost) and (tabuList[i] != bestSol) and (tabuList[i] != sol):
-                            aspiredSolCost = currCost
-                            idx = i
-                        sol = tabuList[idx]
-                        tabuList.pop(idx)
-                else:
-                    sol = newSol
-        return self.getCost(sol), sol
+        canvas = FigureCanvasTkAgg(fig, master = graph)  
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+        toolbar = NavigationToolbar2Tk(canvas,graph)
+        toolbar.update()
+        canvas.get_tk_widget().pack()
+        label1.config(text = o[0])
+        label2.config(text = '[type of transport, city id, time spent in city]')
+        label3.config(text = o[2])
 
 
-def main():
-    opt = Optimize()
-    opt.addVehicle(Vehicle('bus', 100, 1))
-    opt.addVehicle(Vehicle('train', 160, 1.2))
-    opt.addVehicle(Vehicle('plane', 800, 5))
-
-    # [bus, train, plane] order in timetable
-
-    # adding cities to object
-    with open(f'{os.getcwd()}/src/data/cities.json', 'r') as f:
-        data = json.load(f)
-
-    for i in data:
-        opt.addCity(i, np.random.randint(low=2, high=13),
-                    np.random.randint(low=1, high=11))
-
-    with open(f'{os.getcwd()}/src/data/timetable.json', 'r') as f:
-        data = json.load(f)
-
-    for i in range(len(data)):
-        opt.cities[i].addTimetable(data[i]["timetable"])
-
-    opt.calculateDist()
-
-    randTimetable()
-
-    opt.loadTimetable()
-
-    ex = [(0, 0, 0), (0, 2, 1), (0, 5, 3),
-          (2, 3, 2), (2, 10, 0), (1, 1, 0)]
-
-    # print(opt.getCost(ex))
-    # print(opt.cities[0].timetable[0][5])
-    # print(opt.getInitSol(3, 10, 5))
-    # s = opt.getInitSol(3, 10, 5)
-    # print(opt.getNeighborhood(s))
-    # print(opt.correctNeighborhood(opt.getNeighborhood(s), 5))
-    print(opt.opt(100, 5, 3, 1000, 3))
+optimizeBtn=tk.Button(root,text = 'Optimize', command = lambda:optimize())
 
 
-main()
+startPointlabel.grid(row=0,column=0)
+startPointEntry.grid(row=0,column=1)
+endPointlabel.grid(row=1,column=0)
+endPointEntry.grid(row=1,column=1)
+startTimelabel.grid(row=2, column=0)
+startTimeEntry.grid(row=2, column=1)
+maxCostlabel.grid(row=3, column=0)
+maxCostEntry.grid(row=3, column=1)
+maxTabooLenlabel.grid(row=4, column=0)
+maxTabooLenEntry.grid(row=4, column=1)
+aspirationlabel.grid(row=5, column=0)
+aspirationEntry.grid(row=5, column=1)
+nmaxlabel.grid(row=6, column=0)
+nmaxEntry.grid(row=6, column=1)
+
+editSaveBtn.grid(row=7,column=0)
+useSaveBtn.grid(row=7,column=1)
+
+
+busVlabel.grid(row=0,column=4)
+busVEntry.grid(row=0,column=5)
+busCostlabel.grid(row=1, column=4)
+busCostEntry.grid(row=1, column=5)
+trainVlabel.grid(row=2, column=4)
+trainVEntry.grid(row=2, column=5)
+trainCostlabel.grid(row=3,column=4)
+trainCostEntry.grid(row=3,column=5)
+planeVlabel.grid(row=4, column=4)
+planeVEntry.grid(row=4, column=5)
+planeCostlabel.grid(row=5, column=4)
+planeCostEntry.grid(row=5, column=5)
+
+generateBtn.grid(row=6,column=5)
+
+optimizeBtn.grid(row=7,column=3)
+
+label1 = tk.Label(text='')
+label2 = tk.Label(text='')
+label3 = tk.Label(text='')
+label1.grid(row=8,column=3)
+label2.grid(row=9,column=3)
+label3.grid(row=10,column=3)
+
+root.mainloop()
+
+# TODO
+# vehicles with def value
+# btn to generate rand timetable
+# max cost
+# max taboo len
+# aspiration
+# nmax
+# start time
+# optimize()
+# graphs
+# add cities?
